0d2b649d429bb19611027364c77c414de287ea9b
Matheus Soares Camacho <matheus_camacho27@hotmail.com>
2023-01-15 20:35:42 -0300

    Teste Commit

--- src/main/java/hsl/matfox/Main.java
+++ src/main/java/hsl/matfox/Main.java
@@ -1,12 +1,16 @@
 package hsl.matfox;
 
+import hsl.matfox.events.onPlayerJoin;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getServer().getPluginManager().registerEvents(new onPlayerJoin(),this);
 
     }
 
--- src/main/java/hsl/matfox/events/onPlayerJoin.java
+++ src/main/java/hsl/matfox/events/onPlayerJoin.java
@@ -1,10 +1,43 @@
 package hsl.matfox.events;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class onPlayerJoin implements Listener {
-    public void onJoin(PlayerJoinEvent event){
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
 
+public final class onPlayerJoin implements Listener {
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        FileConfiguration config = new YamlConfiguration();
+        try {
+            System.out.println("Teleportado");
+            config.load(new File("src/main/resources/plugin.yml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+        String worldName = config.getString("spawn-location.world");
+        double x = config.getDouble("spawn-location.x");
+        double y = config.getDouble("spawn-location.y");
+        double z = config.getDouble("spawn-location.z");
+        World world = Bukkit.getWorld(worldName);
+        Location spawnLocation = new Location(world, x, y, z);
+
+        Player player = event.getPlayer();
+
+        player.teleport(spawnLocation);
+
+        System.out.println("Teleportado");
     }
 }
